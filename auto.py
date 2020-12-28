537831b88fc2ffc5a28a221330f4ec8f3dfd92bf
慕寒州 <40469011+muhz9786@users.noreply.github.com>
2020-12-28 18:42:57 +0800

    update

--- auto.py
+++ auto.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from easylog import log
 import time as _time
 import json
 import re
